4d8f5d1816a769291d60f96c3ad297edd29191de
csi.bruce <csi.bruce@gmail.com>
2017-08-13 20:39:28 +0900

    add volume controll

--- player.js
+++ player.js
@@ -2,10 +2,12 @@ const { spawn } = require('child_process');
 var player;
 var reservedSongs = [];
 var nowPlayingIndex = 0;
+var rotate = true;
+var volume = -9;
 
 function play(name) {
   player = spawn('omxplayer', [__dirname + '/songs/' + name]);
-
+  volume = -9;
   player.stdout.on('data', function(data) {
     console.log('=========================stdout');
     console.log(data.toString());
@@ -18,9 +20,12 @@ function play(name) {
 
   player.on( 'close', code => {
     console.log(`player process exited with code ${code}`);
-    if (!reservedSongs[nowPlayingIndex]) return;
-    play(reservedSongs[nowPlayingIndex]);
+    if (!rotate && !reservedSongs[nowPlayingIndex]) return;
     nowPlayingIndex++;
+    if (rotate && (reservedSongs.length > 0) && !reservedSongs[nowPlayingIndex]) {
+      nowPlayingIndex = 0;
+    }
+    play(reservedSongs[nowPlayingIndex]);
   });
 }
 
@@ -40,6 +45,14 @@ function playSong(filename) {
 playSong.controll = function(command) {
   if (player) {
     console.log('controlled  = ' + command);
+    switch (command) {
+      case '+':
+        volume+=3;
+        break;
+      case '-':
+        volume-=3;
+        break;
+    }
     player.stdin.write(command);
   }
 }
@@ -47,7 +60,9 @@ playSong.controll = function(command) {
 playSong.list = () => {
   return {
     reservedSongs: reservedSongs,
-    nowPlayingIndex: nowPlayingIndex
+    nowPlayingIndex: nowPlayingIndex,
+    rotate: rotate,
+    volume: volume,
   }
 }
 
